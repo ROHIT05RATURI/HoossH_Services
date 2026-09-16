67c40660bc0ca8ec666371d5c9c4cdfb9a95ea36
Anchal Rauthan <anchalanchurauthan@gmail.com>
2026-09-16 18:13:39 +0530

    commit

---
+++ HoossH_Service_DAL/Models/Attendance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HoossH_Service_DAL.Models
+{
+    public class Attendance
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime Date { get; set; } = DateTime.UtcNow.Date;
+        public TimeSpan? CheckInTime { get; set; }
+        public TimeSpan? CheckOutTime { get; set; }
+        public string Status { get; set; } = "Present"; 
+    }
+}
---
+++ HoossH_Service_DAL/Models/LeaveRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HoossH_Service_DAL.Models
+{
+    public class LeaveRequest
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public string LeaveType { get; set; } = "Casual"; // Casual, Sick, Annual
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Reason { get; set; }
+        public string Status { get; set; } = "Pending";
+    }
+}
